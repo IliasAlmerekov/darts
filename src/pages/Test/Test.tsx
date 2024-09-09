9207ec9aac063d38ea6c00421b642fc19ce69e98
Ilias Teknis <ilias.teknis@db-n.com>
2024-09-09 10:49:56 +0200

    feat(library): general updates DBNHAMDARTAPP-15

---
+++ src/pages/Test/Test.tsx
@@ -0,0 +1,12 @@
+import DefaultInputField from "../../components/DefaultInputField"
+import PlayerInputButton from "../../components/PlayerInputButton"
+
+function Test() {
+    return (
+        <div>
+            <DefaultInputField />
+            <PlayerInputButton />
+        </div>
+    )
+}
+export default Test
