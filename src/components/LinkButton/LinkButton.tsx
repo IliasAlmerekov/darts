c1423569b52771abb70ae2e944e0660c349b73d9
Ilias Teknis <ilias.teknis@db-n.com>
2024-09-16 15:38:32 +0200

    feat(UnselectedPlayerlistOverflow): overflow with box shadow DBNHAMDARTAPP-23

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -5,9 +5,10 @@ type Props = {
     icon: string
     label: string
     handleClick: () => void;
+    className: string;
 }
 
 function LinkButton({ ...props }: Props) {
-    return (<a href={props.href} onClick={props.handleClick} className="link" ><img src={props.icon} alt="" />{props.label}</a>)
+    return (<a href={props.href} onClick={props.handleClick} className={props.className} ><img src={props.icon} alt="" />{props.label}</a>)
 }
 export default LinkButton
