c4b628ebbff89b1c4189ebb060655035f9acd1c9
Ilias Almerekov <ilias.almerekov@db-n.com>
2024-12-02 14:46:19 +0100

    Gamesummary page refactoring

--- src/pages/gamesummary/Gamesummary.tsx
+++ src/pages/gamesummary/Gamesummary.tsx
@@ -1,71 +1,64 @@
-import OverviewPlayerItemList from "../../components/OverviewPlayerItem/OverviewPlayerItemList"
-import './gamesummary.css'
+import OverviewPlayerItemList from "../../components/OverviewPlayerItem/OverviewPlayerItemList";
+import "./gamesummary.css";
 import Button from "../../components/Button/Button";
 import Podium from "../../components/Podium/Podium";
-import Undo from '../../icons/undolinkbutton.svg'
+import Undo from "../../icons/undolinkbutton.svg";
 import { Link } from "react-router-dom";
 import { Dispatch, SetStateAction } from "react";
 
 type Props = {
-    list: BASIC.PlayerProps[]
-    setUndo: Dispatch<SetStateAction<boolean>>
-}
+  list: BASIC.PlayerProps[];
+  setUndo: Dispatch<SetStateAction<boolean>>;
+};
 
 function Gamesummary({ list, setUndo }: Props) {
-    const newList = [...list]
-    const podiumList = newList.slice(0, 3)
-    const leaderboarList = newList.slice(3, list.length + 1)
-    const podiumListWithPlaceholder = [...podiumList]
-    podiumListWithPlaceholder.push({
-        id: 0,
-        name: "-",
-        score: 0,
-        isActive: false,
-        index: 0,
-        rounds: [
-            { throw1: undefined, throw2: undefined, throw3: undefined }
-        ]
-    })
+  const newList = [...list];
+  const podiumList = newList.slice(0, 3);
+  const leaderBoardList = newList.slice(3, list.length + 1);
+  const podiumListWithPlaceholder = [...podiumList];
+  podiumListWithPlaceholder.push({
+    id: 0,
+    name: "-",
+    score: 0,
+    isActive: false,
+    index: 0,
+    rounds: [{ throw1: undefined, throw2: undefined, throw3: undefined }],
+  });
+  const podiumData =
+    podiumList.length === 2 ? podiumListWithPlaceholder : podiumList;
 
-    return (
-        <div className="summary">
-            <div>
-                <Link to="/game" className="undoButton">
-                    <img src={Undo} alt="" onClick={() => setUndo(true)} />
+  return (
+    <div className="summary">
+      <div>
+        <Link to="/game" className="undoButton" onClick={() => setUndo(true)}>
+          <img src={Undo} alt="Undo last action" />
+        </Link>
+      </div>
+      <Podium userMap={podiumData} list={list} />
+      <div className="leaderBoard">
+        <OverviewPlayerItemList userMap={leaderBoardList} />
+      </div>
 
-                </Link>
-            </div>
-            <Podium
-                userMap={(podiumList.length === 2 ? podiumListWithPlaceholder : podiumList)}
-                list={list}
-            />
-            <div className="leaderBoard">
-                <OverviewPlayerItemList
-                    userMap={leaderboarList} />
-            </div>
-
-            <div className="playAgainButton">
-                <Button
-                    isLink
-                    link={"/game"}
-                    label="Play Again"
-                    type="primary"
-                    isInverted
-                    className="playAgainButton"
-                />
-            </div>
-            <div className="backToStartButton">
-                <Button
-                    className="backToStartButton"
-                    link={"/"}
-                    isLink
-                    label="Back To Start"
-                    type="primary"
-
-                />
-            </div>
-
-        </div >
-    )
+      <div className="playAgainButton">
+        <Button
+          isLink
+          link={"/game"}
+          label="Play Again"
+          type="primary"
+          isInverted
+          className="playAgainButton"
+        />
+      </div>
+      <div className="backToStartButton">
+        <Button
+          className="backToStartButton"
+          link={"/"}
+          isLink
+          label="Back To Start"
+          type="primary"
+        />
+      </div>
+    </div>
+  );
 }
-export default Gamesummary
+export default Gamesummary;
