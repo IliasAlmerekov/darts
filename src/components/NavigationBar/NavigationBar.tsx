c0e3763600a2a5ec7cb54ee7d99ac947d04ef3fd
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-12-03 11:25:21 +0100

    fix: update game state handling and API calls

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import settingsCogInactive from "../../icons/settings-inactive.svg";
 import settingsCog from "../../icons/settings.svg";
 import dartIcon from "../../icons/dart.svg";
@@ -7,51 +8,63 @@ import statisticIcon from "../../icons/statistics.svg";
 import statisticIconInactive from "../../icons/statistics-inactive.svg";
 import "./NavigationBar.css";
 import Madebydeepblue from "../../icons/madeByDeepblue.svg";
+import clsx from "clsx";
 
 export default function NavigationBar(): React.JSX.Element {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const navItems = [
     {
       label: "Statistics",
       activeIcon: statisticIcon,
       inActiveIcon: statisticIconInactive,
       id: "statistics",
+      path: "/statistics",
     },
     {
       label: "Game",
       activeIcon: dartIcon,
       inActiveIcon: dartIconInactive,
       id: "game",
+      path: "/start",
     },
     {
       label: "Settings",
       activeIcon: settingsCog,
       inActiveIcon: settingsCogInactive,
       id: "settings",
+      path: "/settings",
     },
   ];
+
+  const handleTabClick = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <div className="navigation">
       <img className="deepblue-icon" src={Madebydeepblue} alt="" />
-      {navItems.map((item) => (
-        <button
-          key={item.id}
-          /*onClick={() => functions.handleTabClick(item.id, navigate)}
-          className={clsx("tab-button", {
-            active: event.activeTab === item.id,
-            inactive: !(event.activeTab === item.id),
-          })}*/
-        >
-          <span>
-            <img
-              src={item.activeIcon}
-              alt={item.label}
-              /*src={item.id === event.activeTab ? item.activeIcon : item.inActiveIcon}
-              alt={item.label}*/
-            />
-            {item.label}
-          </span>
-        </button>
-      ))}
+      {navItems.map((item) => {
+        const isActive =
+          location.pathname === item.path || (item.id === "game" && location.pathname === "/start");
+
+        return (
+          <button
+            key={item.id}
+            onClick={() => handleTabClick(item.path)}
+            className={clsx("tab-button", {
+              active: isActive,
+              inactive: !isActive,
+            })}
+          >
+            <span>
+              <img src={isActive ? item.activeIcon : item.inActiveIcon} alt={item.label} />
+              {item.label}
+            </span>
+          </button>
+        );
+      })}
     </div>
   );
 }
