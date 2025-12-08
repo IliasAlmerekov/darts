2b7b9591763b4038b5998614f2527d8c6cfeaeab
Ilias Almerekov <ilias.almerekov@db-n.com>
2025-12-08 10:11:06 +0100

    refactor: fix any bugs and rewrite with fsd

--- src/components/game-player-item/FinishedGamePlayerItemList.tsx
+++ src/components/game-player-item/FinishedGamePlayerItemList.tsx
@@ -1,44 +1,5 @@
 import styles from "./GamePlayerItem.module.css";
 
-type FinishedPlayerItem = {
-  name: string;
-  position?: number | null;
-};
-
-type Props = {
-  userMap?: FinishedPlayerItem[];
-};
-
-function FinishedGamePlayerItemList({ userMap }: Props) {
-  const FinishedGamePlayerItem = ({ name, place }: { name?: string; place?: string }) => (
-    <div className={`${styles.gamePlayerItem} ${styles.finished}`}>
-      <div>
-        <div className={styles.copylarge}>{name}</div>
-      </div>
-      <div className={styles.place}>{place}</div>
-    </div>
-  );
-
-  if (!userMap || userMap.length === 0) {
-    return <></>;
-  }
-
-  return (
-    <div className={styles.finishedPlayerList}>
-      <div className={`${styles.copylarge} ${styles.finishedPlayers}`}>Finished Players</div>
-      {userMap.map((item, index) => {
-        // Use backend position if available, otherwise use order in array
-        const place = item.position != null && item.position > 0 ? item.position : index + 1;
-        return <FinishedGamePlayerItem key={index} name={item.name} place={`${place}.`} />;
-      })}
-    </div>
-  );
-}
-
-export default FinishedGamePlayerItemList;
-
-import styles from "./GamePlayerItem.module.css";
-
 type Props = {
   userMap?: { name: string }[];
 };
